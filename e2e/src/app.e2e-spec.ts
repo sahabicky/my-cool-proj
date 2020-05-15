f18297a06dc0c47a4e657b18485effde1ecedc8a
bikisaha17@yahoo.com <bikisaha17@yahoo.com>
2020-05-15 22:52:02 +0530

    Test cases executed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-cool-proj app is running!');
+    expect(page.getTitleText()).toEqual('my-cool-proj');
   });
 
   afterEach(async () => {
